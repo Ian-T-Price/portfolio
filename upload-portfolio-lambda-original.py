981e3fd2ba1f26de35db6dd09f567f1874b3c2e8
Ian T Price <ian.t.price@gmail.com>
2017-09-18 23:42:55 +0100

    Added AWS lambda version of the upload.py

---
+++ upload-portfolio-lambda-original.py
@@ -0,0 +1,34 @@
+"""
+S3 Zipfile Upload script.
+
+Script to upload from a zip in an S3 build bucket to a deploy bucket
+"""
+
+import boto3
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.iantprice.com')
+build_bucket = s3.Bucket('portfoliobuild.iantprice.com')
+portfolio_zip = io.BytesIO()
+
+build_bucket.download_fileobj(
+  'portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        mimetype = mimetypes.guess_type(nm)
+        if mimetype[0] is None:
+            portfolio_bucket.upload_fileobj(obj, nm)
+            print('non-valid Mime Type:', nm)
+        else:
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetype[0]})
+        # This fails on unknown file types .jpeg or .png
+        # portfolio_bucket.upload_fileobj(obj, nm,
+        #    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,26 +9,30 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.iantprice.com')
-build_bucket = s3.Bucket('portfoliobuild.iantprice.com')
-portfolio_zip = io.BytesIO()
+def lambda_handler(event, context):
+    """Upload zip file from GitHub to S3 bucket."""
+    s3 = boto3.resource('s3')
 
-build_bucket.download_fileobj(
-  'portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.iantprice.com')
+    build_bucket = s3.Bucket('portfoliobuild.iantprice.com')
+    portfolio_zip = io.BytesIO()
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        mimetype = mimetypes.guess_type(nm)
-        if mimetype[0] is None:
-            portfolio_bucket.upload_fileobj(obj, nm)
-            print('non-valid Mime Type:', nm)
-        else:
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetype[0]})
-        # This fails on unknown file types .jpeg or .png
-        # portfolio_bucket.upload_fileobj(obj, nm,
-        #    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    build_bucket.download_fileobj(
+      'portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            mimetype = mimetypes.guess_type(nm)
+            if mimetype[0] is None:
+                portfolio_bucket.upload_fileobj(obj, nm)
+                print('non-valid Mime Type:', nm)
+            else:
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetype[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print("Latest portfolio files uploaded")
+
+    return 'Portfolio updated'

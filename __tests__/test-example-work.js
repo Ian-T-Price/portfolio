e965e59a1882eb11809401c78ef5c7cbaf903880
Ian T Price <ian.t.price@gmail.com>
2017-10-15 18:53:37 +0100

    Changed examples bubble to be imported from .json file Changed test-example-work data to be imported from .json file

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -1,31 +1,14 @@
 import React from 'react';
+//import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ExampleWork, {ExampleWorkBubble} from '../js/example-work';
+import myWork from '../data/test-example-work.json'
 
 configure({ adapter: new Adapter() });
 
-const myWork = [
-  {
-    'title': "Carlton Carriages WordPress Site",
-    'image': {
-      'desc': "Screenshot of a Carlton Carriages Landau and four horses outside Buckingham Palace",
-      'src': "images/carlton-carriages_buckingham-palace_640x350.jpg",
-      'alt': "Screenshot of a Carlton Carriages Landau and four horses outside Buckingham Palace",
-      'comment': ""
-    }
-  },
-  {
-    'title': "WordPress, DevOps Styly",
-    'image': {
-      'desc': "screenshot of a WordPress project using DevOps principles",
-      'src': "images/example2.png",
-      'alt': "screenshot of a WordPress project using DevOps principles",
-      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
-                  https://www.flickr.com/photos/ssoosay/4097410999`
-    }
-  }
-];
+//const myWork = require('json-loader!../data/test-example-work.json');
+//const myWork = require('json-loader!../data/test-example-work.json');
 
 describe("ExampleWork component", () => {
   let mockCloseModalFn = jest.fn();
--- js/main.js
+++ js/main.js
@@ -1,44 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
+import myWork from '../data/examples.json'
 
-const myWork = [
-  {
-    'title': "Carlton Carriages WordPress Site",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'image': {
-      'desc': "Screenshot of a Carlton Carriages Landau and four horses outside Buckingham Palace",
-      'src': "images/carlton-carriages_buckingham-palace_640x350.jpg",
-      'alt': "Screenshot of a Carlton Carriages Landau and four horses outside Buckingham Palace",
-      'comment': ""
-    }
-  },
-  {
-    'title': "WordPress, DevOps Styly",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'image': {
-      'desc': "screenshot of a WordPress project using DevOps principles",
-      'src': "images/example2.png",
-      'alt': "screenshot of a WordPress project using DevOps principles",
-      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
-                  https://www.flickr.com/photos/ssoosay/4097410999`
-    }
-  },
-  {
-    'title': "Serverless: Function As A Service",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'image': {
-      'desc': "screenshot of a project involving Serverless Architecture",
-      'src': "images/example3.png",
-      'alt': "screenshot of a project involving Serverless Architecture",
-      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
-                  https://www.flickr.com/photos/37287295@N00/2540855181`
-    }
-  }
-]
+//const myWork = require('json-loader!../data/examples.json');
 
 ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
 
